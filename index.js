b09ec55b768e2ae6100ed7c873d3881d67a6df93
Katrin <ekaterinapashkovich000@gmail.com>
2026-04-20 21:50:33 +0300

    проверка обновлений

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 "use strict";
-let num = '2';
-switch (num){
-  case 1:
-  alert ('Нуи ну');
-  case '2':
-  alert ('Ну тебя!');
-  case 4:
-  alert ('Наконец');
-  case 5:
-  alert ('Только не это')
-}
+
 function(num){
   if(num === 0){
     alert(`hello`)
